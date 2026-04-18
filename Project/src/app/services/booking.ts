8821c48f4cf6ec0bac5d7e8d1a2a29da433dcf52
Aksungkar <aksunkarmyktybek@gmail.com>
2026-04-18 22:21:52 +0500

    feat: created hotel and booking services

--- Project/src/app/services/booking.ts
+++ Project/src/app/services/booking.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, signal } from '@angular/core';
+import { Booking } from '../models/booking.model';
 @Injectable({
   providedIn: 'root',
 })
-export class Booking {
-  
+export class BookingService {
+  bookingData = signal<Booking | null>(null)
+
+  book(data: Booking) {
+    this.bookingData.set(data);
+  }
+
+  clearBooking() {
+    this.bookingData.set(null);
+  }
 }
--- Project/src/app/services/hotel.ts
+++ Project/src/app/services/hotel.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, signal } from '@angular/core';
+import { Hotel } from '../models/hotel.model';
 @Injectable({
   providedIn: 'root',
 })
-export class Hotel {
+export class HotelService {
   
+  selectedHotel = signal<Hotel | null>(null)
+
+  // just for test
+  getMockHotel(): Hotel {
+    return {
+      id: 1,
+      name: "BigHotel",
+      location: "Almaty",
+      price: 49000,
+      num_of_rooms: 3,
+      description: "Big hotel at the center of city",  
+      images: ["https://parkhotel-almaty.com/", "https://travel.yandex.ru/hotels/almaty/"],
+      facilities: ["Parking", "wifi"],
+      rating: 5.0,
+    }
+  }
 }
