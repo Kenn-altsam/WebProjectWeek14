235a7803273fc54179828f441ab95c7613c32812
Murat Alikhan <alihanmurat44@gmail.com>
2026-04-19 23:04:48 +0500

    half of footer

--- Project/src/app/pages/home/home.ts
+++ Project/src/app/pages/home/home.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {Navbar} from '../../components/navbar/navbar';
 import {Devider} from '../../components/devider/devider';
 import {SearchBar} from '../../components/search-bar/search-bar';
+import {Footer} from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home',
   imports: [
     Navbar,
     Devider,
-    SearchBar
+    SearchBar,
+    Footer
   ],
   templateUrl: './home.html',
   styleUrl: './home.css',
