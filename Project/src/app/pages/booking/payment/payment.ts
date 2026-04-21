18e2c43e09b2ce2470ee42cdef5667198c7aa483
Kenn-altsam <a.kenzhe.sam@gmail.com>
2026-04-21 23:41:01 +0500

    updated payment.ts, payment.html so that it would work with backend

--- Project/src/app/pages/booking/payment/payment.ts
+++ Project/src/app/pages/booking/payment/payment.ts
@@ -1,14 +1,17 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { BookingService } from '../../../services/booking';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
-  imports: [RouterLink, FormsModule],
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './payment.html',
   styleUrl: './payment.css',
 })
+
 export class Payment implements OnInit {
   bookingService = inject(BookingService);
   router = inject(Router);
@@ -19,6 +22,8 @@ export class Payment implements OnInit {
   bank = signal('');
   expDate = signal('');
   cvv = signal('');
+  errorMessage = signal('');
+  isSubmitting = signal(false);
 
   ngOnInit() {
     if (!this.booking()) {
@@ -26,18 +31,39 @@ export class Payment implements OnInit {
     }
   }
 
-  errorMessage = signal('');
-
   pay() {
+
+    const bookingData = this.booking();
+
+    if(!bookingData) {
+      this.errorMessage.set('No booking data found!');
+      this.router.navigate(['/booking']);
+      return;
+    }
+
     if (!this.cardNumber() || !this.bank() || !this.expDate() || !this.cvv()) {
       this.errorMessage.set('Please fill all fields!');
       return;
     }
+    
     if (this.cardNumber().length < 16) {
       this.errorMessage.set('Card number must be 16 digits!');
       return;
     }
+
     this.errorMessage.set('');
-    this.router.navigate(['/payment-success']);
+    this.isSubmitting.set(true);
+
+    this.bookingService.createBooking(bookingData).subscribe({
+      next: () => {
+        this.bookingService.clearBooking();
+        this.isSubmitting.set(false);
+        this.router.navigate(['/payment-success']);
+      },
+      error: () => {
+        this.isSubmitting.set(false);
+        this.errorMessage.set('Payment failed. Please try again.');
+      }
+    });
   }
 }
