6464de2ff53f8095a7ba5c46253be792e1946e5f
Kenn-altsam <a.kenzhe.sam@gmail.com>
2026-04-22 04:19:39 +0500

    added the ability to go to Login page with Login button, and Registration page with Create Account button

--- Project/src/app/pages/auth/register-user/register-user.ts
+++ Project/src/app/pages/auth/register-user/register-user.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './register-user.html',
   styleUrls: ['./register-user.css']
 })
