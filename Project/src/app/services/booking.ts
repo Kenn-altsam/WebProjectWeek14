6681a3ff44b182b67493020c5f9703d7fc7c7e2f
Kenn-altsam <a.kenzhe.sam@gmail.com>
2026-04-21 18:10:28 +0500

    upd booking.ts so that it would work with backend api

--- Project/src/app/services/booking.ts
+++ Project/src/app/services/booking.ts
@@ -1,16 +1,30 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Booking } from '../models/booking.model';
+
 @Injectable({
   providedIn: 'root',
 })
 export class BookingService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://127.0.0.1:8000/api/bookings';
+
   bookingData = signal<Booking | null>(null)
 
-  book(data: Booking) {
+  setBookingData(data: Booking): void {
     this.bookingData.set(data);
   }
 
-  clearBooking() {
+  createBooking(data: Booking): Observable<Booking> {
+    return this.http.post<Booking>(`${this.apiUrl}/`, data);
+  }
+
+  getBookingData(): Booking | null {
+    return this.bookingData();
+  }
+
+  clearBooking(): void {
     this.bookingData.set(null);
   }
 }
